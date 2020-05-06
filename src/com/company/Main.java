f734558b12b2dbeeb419d5d806064bd4b402ed46
NicolasMitre <nicolasmelluso7@gmail.com>
2020-05-06 15:37:12 -0300

    [Refactor] apply changes recommended by Fausto Moya

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,26 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ArrayList <String> arrayList = new ArrayList<>();
+        String a[] = new String[] { "No", "Te", "Olvides", "De", "Poner", "El", "Where", "En", "El", "Delete", "From"};
 
-        arrayList.add("No");
-        arrayList.add("Te");
-        arrayList.add("Olvides");
-        arrayList.add("De");
-        arrayList.add("Poner");
-        arrayList.add("El");
-        arrayList.add("Where");
-        arrayList.add("En");
-        arrayList.add("El");
-        arrayList.add("Delete");
-        arrayList.add("From");
+        List<String> words = Arrays.asList(a);
 
-        System.out.println(removeEvenLength(arrayList));
-
+        System.out.println(removeEvenLength(words));
     }
 
-    public static ArrayList<String> removeEvenLength(ArrayList<String> arrayList) {
+    public static List<String> removeEvenLength(List<String> words) {
         ArrayList <String> arrayNewList = new ArrayList<>();
 
-       for(int i = 0; i < arrayList.size(); i++){
-            if (arrayList.get(i).length() % 2 != 0){
-                arrayNewList.add(arrayList.get(i));
+       for(String word : words){
+            if (word.length() % 2 != 0){
+                arrayNewList.add(word);
             }
         }
 
